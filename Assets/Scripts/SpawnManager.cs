eeb91774b0a16e3a8e9db1a0f585c471e49f463e
Simon T. <simtho0624@gmail.com>
2025-02-05 15:46:38 +0100

    Add diff animal spawns

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
-    public int animalIndex;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,6 +14,8 @@ public class SpawnManager : MonoBehaviour
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.S)){
+
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
             Instantiate(animalPrefabs[animalIndex], new Vector3(0,0,20), animalPrefabs[animalIndex].transform.rotation);
         }
     }
