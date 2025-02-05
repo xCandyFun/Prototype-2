6c007f00d2ddc0ea0a78bead2ab83a341f866238
Simon T. <simtho0624@gmail.com>
2025-02-05 13:14:30 +0100

    Rename the class

--- Assets/Scripts/MoveForward.cs
+++ Assets/Scripts/MoveForward.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class FoodMovingForward : MonoBehaviour
+public class MoveForward : MonoBehaviour
 {
     public float speed = 40.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
