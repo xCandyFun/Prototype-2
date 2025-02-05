036250786309c14b8398e5deba65c8ec767e4a46
Simon T. <simtho0624@gmail.com>
2025-02-05 12:50:58 +0100

    Update gitignore and add food move forward

---
+++ Assets/Scripts/FoodMovingForward.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class FoodMovingForward : MonoBehaviour
+{
+    public float speed = 40.0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+}
