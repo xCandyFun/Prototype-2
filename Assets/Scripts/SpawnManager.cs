a6de807c4578ff34c97b4c1de8d0a2f9454d749c
Simon T. <simtho0624@gmail.com>
2025-02-06 12:54:50 +0100

    Add invokeRepeating

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,20 +5,19 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] animalPrefabs;
     private float spawnRangeX = 15.0f;
     private float spawnRangeZ = 15.0f;
+    private float startDelay = 2.0f;
+    private float spawnInterval = 1.5f;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimals", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.S)){
-
-            SpawnRandomAnimals();
-        }
+    
     }
 
     void SpawnRandomAnimals() {
